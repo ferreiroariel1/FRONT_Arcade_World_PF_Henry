9ee48cea52bdff5f216ab6fffe14464b679faf8d
luvihu <luvillogas@gmail.com>
2023-10-28 10:38:28 -0500

    modif

--- src/views/home/components/MoreDownload.jsx
+++ src/views/home/components/MoreDownload.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import '../style/DStyle.css'
 
 function MoreDownload({name,image,price}) {
   return (
-    <div>
-        <img src={image} alt="" style={{width:'12em'}}/>
-    <h5>{name}</h5>
-    <p>{price}</p>
+    <div className='cards'>
+        <img className='images' src={image} alt="" />
+    <div className='TPS'>
+    <h5 className='titles'>{name}</h5>
+    <p className='prices'>$/{price}</p>
+    </div>
     </div>
   )
 }
