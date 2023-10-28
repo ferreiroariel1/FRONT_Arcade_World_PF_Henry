f43b76da40f15570ff0d17787791e0926c0c7cb4
luvihu <luvillogas@gmail.com>
2023-10-28 18:19:50 -0500

    algo

--- src/views/details/details.jsx
+++ src/views/details/details.jsx
@@ -5,7 +5,7 @@ const Details = () =>{
      
    return(
     <>
-
+<br />
     <div className={style.container}>
 <div>
     <img className={style.photo} src="https://m.media-amazon.com/images/I/81KUccM8azL._AC_UF1000,1000_QL80_.jpg" alt="" />
